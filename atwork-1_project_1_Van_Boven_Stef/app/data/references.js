5561231a3e9d27d94aa7aaab80be4e7358db69b3
pgm-stefvanboven <113516199+pgm-stefvanboven@users.noreply.github.com>
2022-11-24 10:57:39 +0100

    added excersice 1
    added excersice 1, the folder structure, all the needed files and the fontsizes

---
+++ atwork-1_project_1_Van_Boven_Stef/app/data/references.js
@@ -0,0 +1,62 @@
+const REFERENCES = [
+  {
+    id: "e7d9be12-df57-47f7-8346-626a023b497",
+    type: "b2b",
+    image: "b2b_1.jpg",
+    description: "Frans zakenkantoor",
+  },
+  {
+    id: "3435b895-21ce-48d1-8827-1dc9705dc20c",
+    type: "private",
+    image: "private_1.jpg",
+    description: "Huisje in de Ardennen",
+  },
+  {
+    id: "d1e0a4fd-3c12-4fe6-9f63-2a10f7b8cb07",
+    type: "private",
+    image: "private_2.jpg",
+    description: "Opgehoogde tuin in Denemarken",
+  },
+  {
+    id: "81f7c046-49a3-4a3b-a30d-597cf5ffb1e1",
+    type: "public",
+    image: "public_4.jpg",
+    description: "Aanleg nieuw bloemenveld in Parijs",
+  },
+  {
+    id: "35f9c738-b81c-4bfc-9bbb-d8c1521a2b16",
+    type: "public",
+    image: "public_1.jpg",
+    description: "Onderhoud Japanse tuin in park",
+  },
+  {
+    id: "d01a0e5c-c002-456e-ae50-7ef74bf8674a",
+    type: "b2b",
+    image: "b2b_2.jpg",
+    description: "Hobbit gemeenschap in Nieuw-Zeeland",
+  },
+  {
+    id: "3be0d873-1a16-4de0-b8b6-79c0dffa12e1",
+    type: "b2b",
+    image: "b2b_3.jpg",
+    description: "Aanleg doolhoftuin voor vastgoedkantoor",
+  },
+  {
+    id: "6b48157c-f351-4406-8db4-eaaae516474d",
+    type: "public",
+    image: "public_2.jpg",
+    description: "Aanleg en onderhoud Colour Garden (UK)",
+  },
+  {
+    id: "9fd57650-66a8-4016-99dc-f8d5b3dd9aa7",
+    type: "private",
+    image: "private_3.jpg",
+    description: "Tuin voor klant in Ibiza",
+  },
+  {
+    id: "9bf1d6aa-f7b7-437a-af9c-bee96667d11b",
+    type: "public",
+    image: "public_3.jpg",
+    description: "Onderhoud park rond Eifeltoren",
+  },
+];
